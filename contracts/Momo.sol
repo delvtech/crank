deadfcd68edafeb51fbc256631db3c4ba5a7e43e
Alex Towle <jalextowle@gmail.com>
2024-09-14 00:44:32 -0500

    Made Momo a ERC4626 vault

--- contracts/Momo.sol
+++ contracts/Momo.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.22;
 
+import { IERC20 } from "hyperdrive/src/interfaces/IERC20.sol";
 import { IHyperdrive } from "hyperdrive/src/interfaces/IHyperdrive.sol";
+import { ERC4626 } from "solady/tokens/ERC4626.sol";
 import { IMomo } from "./interfaces/IMomo.sol";
 import { MOMO_KIND, VERSION } from "./libraries/Constants.sol";
 
@@ -12,7 +14,7 @@ import { MOMO_KIND, VERSION } from "./libraries/Constants.sol";
 /// @custom:disclaimer The language used in this code is for coding convenience
 ///                    only, and is not intended to, and does not, have any
 ///                    particular legal or regulatory significance.
-contract Momo is IMomo {
+contract Momo is IMomo, ERC4626 {
     // ╭─────────────────────────────────────────────────────────╮
     // │ Storage                                                 │
     // ╰─────────────────────────────────────────────────────────╯
@@ -33,16 +35,22 @@ contract Momo is IMomo {
     /// @notice The Hyperdrive pool to buy shorts from.
     IHyperdrive public immutable shortPositionSource;
 
+    /// @dev The decimals of the Momo token.
+    uint8 internal immutable _decimals;
+
+    /// @dev The asset underlying all of the investments.
+    IERC20 internal immutable _asset;
+
     // ─────────────────────────── State ────────────────────────
 
-    /// @dev Name of the Momo token.
-    string public name;
-
-    /// @dev Symbol of the Momo token.
-    string public symbol;
-
-    /// @dev Address of the contract admin.
+    /// @notice The admin address.
     address public admin;
+
+    /// @dev The name of the Momo token.
+    string internal _name;
+
+    /// @dev The symbol of the Momo token.
+    string internal _symbol;
 
     // ╭─────────────────────────────────────────────────────────╮
     // │ Modifiers                                               │
@@ -61,25 +69,91 @@ contract Momo is IMomo {
     // ╰─────────────────────────────────────────────────────────╯
 
     /// @notice Instantiates the Momo vault.
-    /// @param _name Name of the Momo vault token.
-    /// @param _symbol Symbol of the Momo vault token.
+    /// @param __name Name of the Momo vault token.
+    /// @param __symbol Symbol of the Momo vault token.
+    /// @param __decimals The Momo vault token decimals.
     /// @param _longPositionSource The Hyperdrive pool to buy long positions from.
     /// @param _shortPositionSource The Hyperdrive pool to buy short positions from.
+    /// @param __asset The asset underlying both the long and short sources.
     constructor(
-        string memory _name,
-        string memory _symbol,
+        string memory __name,
+        string memory __symbol,
+        uint8 __decimals,
         IHyperdrive _longPositionSource,
-        IHyperdrive _shortPositionSource
+        IHyperdrive _shortPositionSource,
+        IERC20 __asset
     ) {
         // Set the name and symbol.
-        name = _name;
-        symbol = _symbol;
+        _name = __name;
+        _symbol = __symbol;
+        _decimals = __decimals;
 
         // Set the position sources.
         longPositionSource = _longPositionSource;
         shortPositionSource = _shortPositionSource;
 
+        // TODO: We'll need to generalize this quite a bit to support real
+        // integrations.
+        //
+        // Set the asset.
+        _asset = __asset;
+
         // Set the admin to the contract deployer.
         admin = msg.sender;
+    }
+
+    // ╭─────────────────────────────────────────────────────────╮
+    // │ Getters                                                 │
+    // ╰─────────────────────────────────────────────────────────╯
+
+    // ───────────────────────── ERC20 ───────────────────────────
+
+    /// @notice Returns the name of the Momo token.
+    /// @return The name of the Momo token.
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    /// @notice Returns the symbol of the Momo token.
+    /// @return The symbol of the Momo token.
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+
+    /// @notice Returns the decimals places of the Momo token.
+    /// @return The Momo token decimals.
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    // ───────────────────────── ERC4626 ─────────────────────────
+
+    /// @notice Returns the asset underlying all of the investments.
+    /// @return The asset.
+    function asset() public view override returns (address) {
+        return address(_asset);
+    }
+
+    // TODO: This needs to be updated with the Momo portfolio valuation.
+    //
+    /// @notice Returns the total value of Momo's portfolio measured in the
+    ///         underlying asset.
+    /// @return The total value of Momo's portfolio.
+    function totalAssets() public view override returns (uint256) {
+        return _asset.balanceOf(address(this));
+    }
+
+    /// @dev The number of decimals of the underlying asset.
+    /// @return The number of underlying decimals.
+    function _underlyingDecimals() internal view override returns (uint8) {
+        return _decimals;
+    }
+
+    // TODO: Is this sufficient?
+    //
+    /// @dev The number of decimals of the virtual shares. This helps to avoid
+    ///      inflation attacks.
+    function _decimalsOffset() internal view override returns (uint8) {
+        return _decimals > 6 ? _decimals - 3 : _decimals;
     }
 }
--- contracts/interfaces/IMomo.sol
+++ contracts/interfaces/IMomo.sol
@@ -1,22 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.22;
+pragma solidity ^0.8.20;
+
+import { IERC4626 } from "hyperdrive/src/interfaces/IERC4626.sol";
 
 /// @title IMomo
 /// @author DELV
 /// @notice The Momo vault interface.
-interface IMomo {
+interface IMomo is IERC4626 {
     /// @notice Thrown when an unauthorized user attempts to access admin
     ///         functionality.
     error Unauthorized();
 
-    // FIXME: Can this be pure?
-    //
     /// @notice Gets the kind of the Momo vault.
     /// @return The kind of the Momo vault.
     function kind() external view returns (string memory);
 
-    // FIXME: Can this be pure?
-    //
     /// @notice Gets the version of the Momo vault.
     /// @return The version of the Momo vault.
     function version() external view returns (string memory);
